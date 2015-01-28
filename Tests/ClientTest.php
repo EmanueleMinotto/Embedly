e903dd2a8c901e9f8d82c89898a9cca4240042de
Emanuele Minotto <minottoemanuele@gmail.com>
2015-01-28 21:28:06 +0100

    tests first commit (#2)

---
+++ Tests/ClientTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace EmanueleMinotto\Embedly\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @author Emanuele Minotto <minottoemanuele@gmail.com>
+ *
+ * @coversDefaultClass \EmanueleMinotto\Embedly\Client
+ */
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
